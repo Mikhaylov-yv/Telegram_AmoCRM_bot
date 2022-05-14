68745992fc217a62d168e9b44bc55d8bda4ef017
mikhaylov-yv <dezmond22@yandex.ru>
2022-05-14 17:51:34 +0300

    Сделал основные хендлеры

--- scr/start_bot.py
+++ scr/start_bot.py
@@ -22,11 +22,32 @@ async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    parse_mode='Markdown', text=stat_text)
 
+async def getinfo(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   parse_mode='Markdown', text='Напишите номер вашего заказа')
+
+async def gethelp(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   parse_mode='Markdown', text='Какой вопрос вы хотите задать?')
+
+async def getdoc(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   parse_mode='Markdown', text='Номер документа который необходимо загрузить')
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(json.load(open('telegram_conf.json', 'r'))['token']).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    getinfo_handler = CommandHandler('getinfo', getinfo)
+    application.add_handler(getinfo_handler)
+
+    gethelp_handler = CommandHandler('gethelp', gethelp)
+    application.add_handler(gethelp_handler)
+
+    getdoc_handler = CommandHandler('getdoc', getdoc)
+    application.add_handler(getdoc_handler)
 
     application.run_polling()
