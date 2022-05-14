9ff1df781f6ea9993b49e5f50c6600e2708d1ad4
mikhaylov-yv <dezmond22@yandex.ru>
2022-05-14 17:39:37 +0300

    Изменил текст приветствия

--- scr/start_bot.py
+++ scr/start_bot.py
@@ -8,11 +8,19 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+stat_text = """*Этот бот поможет вам*:
+
+/start Возможности бота
+/getinfo Получить информацию по заказу
+/gethelp Связаться со службой поддержки
+/getdoc Загрузить необходимый документ
+"""
 
 async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     print(update['message']['date'])
     print(update['message']['from'])
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Я бот, пожалуйста, поговорите со мной!")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   parse_mode='Markdown', text=stat_text)
 
 
 if __name__ == '__main__':
