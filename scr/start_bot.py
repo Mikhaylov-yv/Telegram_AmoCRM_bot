401439fa1eff3a9761f1037f930a66a46c865403
mikhaylov-yv <dezmond22@yandex.ru>
2022-05-15 01:12:58 +0300

    Вынес текста сообщений в отдельный файл

--- scr/start_bot.py
+++ scr/start_bot.py
@@ -8,68 +8,65 @@ from telegram.ext import \
     MessageHandler, \
     filters
 import json
-
+from mesag_text import mesags
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
-stat_text = """*Этот бот поможет вам*:
-
-/start Возможности бота
-/getinfo Получить информацию по заказу
-/gethelp Связаться со службой поддержки
-/getdoc Загрузить необходимый документ
-/cancel Закончить общение
-"""
 
 MENU, SEND_ID_INFO, SEND_ANSWER, SEND_DOC = range(4)
 
 async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     logger.info("Пользователь %s начал общение.", update.message.from_user.first_name)
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text=stat_text)
+                                   parse_mode='Markdown', text=mesags['start'])
     return MENU
-
-# async def getcomand(update: Update, context: CallbackContext.DEFAULT_TYPE):
 
 
 async def getinfo(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Напишите номер вашего заказа')
+                                   parse_mode='Markdown', text=mesags['getinfo'])
     return SEND_ID_INFO
+
 
 async def gethelp(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Какой вопрос вы хотите задать?')
+                                   parse_mode='Markdown', text=mesags['gethelp'])
     return SEND_ANSWER
+
 
 async def getdoc(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Номер документа который необходимо загрузить')
+                                   parse_mode='Markdown', text=mesags['getdoc'])
     return SEND_DOC
+
 
 async def send_id_info(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Спасибо вот информация по заказу:')
+                                   parse_mode='Markdown', text=mesags['send_id_info'].format(update.message.text))
     return MENU
+
 
 async def send_answer(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Ваше сообщение переданно в службу поддержки.\nМыскоро с вами свяжемся')
+                                   parse_mode='Markdown', text=mesags['send_answer'].format(update.message.text))
     return MENU
+
 
 async def send_doc(update: Update, context: CallbackContext.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Направляем для вас документ:')
+                                   parse_mode='Markdown', text=mesags['send_doc'].format(update.message.text))
     return MENU
+
 
 async def cancel(update: Update, context: CallbackContext.DEFAULT_TYPE):
     logger.info("User %s canceled the conversation.", update.message.from_user.first_name)
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   parse_mode='Markdown', text='Спасибо за общение!')
+                                   parse_mode='Markdown', text=mesags['cancel'])
     return ConversationHandler.END
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(json.load(open('telegram_conf.json', 'r'))['token']).build()
